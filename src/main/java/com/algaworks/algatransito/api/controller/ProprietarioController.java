f81f00b352a459aea3f2248d022838f3ee65cb1f
Francisco Junior <jrpires.git@gmail.com>
2023-12-01 13:19:20 -0400

    commit inicial

---
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -0,0 +1,69 @@
+package com.algaworks.algatransito.api.controller;
+
+import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
+import com.algaworks.algatransito.domain.service.RegistroProprietarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/proprietarios")
+public class ProprietarioController {
+
+    @Autowired
+    private ProprietarioRepository proprietarioRepository;
+
+    @Autowired
+    private RegistroProprietarioService registroProprietarioService;
+
+    @GetMapping()
+    public List<Proprietario> listar() {
+        return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    public Proprietario cadastrar(@RequestBody @Valid Proprietario proprietario) {
+        return registroProprietarioService.salvar(proprietario);
+    }
+
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId, @RequestBody @Valid Proprietario proprietario) {
+
+        if(!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizado = registroProprietarioService.salvar(proprietario);
+        return ResponseEntity.ok().body(proprietarioAtualizado);
+
+    }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+
+        if(!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        registroProprietarioService.excluir(proprietarioId);
+        return ResponseEntity.noContent().build();
+
+    }
+
+
+
+}
---
+++ src/main/java/com/algaworks/algatransito/domain/repository/ProprietarioRepository.java
@@ -0,0 +1,14 @@
+package com.algaworks.algatransito.domain.repository;
+
+import com.algaworks.algatransito.domain.model.Proprietario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+
+    Optional<Proprietario> findByEmail(String email);
+
+}
